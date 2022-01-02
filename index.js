a9c62f593a299c9970b02c774ba41909701a174d
Airton Cordeiro <airton.cordeiro@bunzlbrasil.protcapad.com.br>
2022-01-01 23:28:00 -0300

    second commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const readlineSync = require('readline-sync')
 
 console.log("Bem vindo ao Bot conversor")
 
@@ -7,8 +8,8 @@ async function robo(){
 
     const browser = await puppeteer.launch({headless : true});
     const page = await browser.newPage();
-    const moedaBase = 'dolar';
-    const moedaFinal = 'real'
+    const moedaBase = readlineSync.question('Informe uma moeda Base: ') || 'dolar';
+    const moedaFinal = readlineSync.question('Informe uma moeda Desejada: ') || 'real'
     const qualquerUrl = `https://www.google.com/search?q=${moedaBase}+para+${moedaFinal}&rlz=1C1GCEU_pt-BRBR976BR976&oq=${moedaBase}+para+${moedaFinal}&aqs=chrome..69i57j0i512l9.6975j1j7&sourceid=chrome&ie=UTF-8` 
     await page.goto(qualquerUrl);
     //await page.screenshot({ path: 'example.png' });
